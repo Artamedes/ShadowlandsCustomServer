cc2038e997d07a52e202aa1cd0c9b5d36da36380
Deveiro <deveirowow@gmail.com>
2022-06-04 03:04:55 -0400

    Added GM guild "Commander" guy in GM Tavern. Added warning message to GM Tailor to let GMs know they risk ban by giving away the robes to players. Added SQL update for the GM Commander Bjorn to be in the world with text.

--- src/server/scripts/Custom/GMTavern.cpp
+++ src/server/scripts/Custom/GMTavern.cpp
@@ -1,5 +1,8 @@
 #include "ScriptMgr.h"
 #include "WorldSession.h"
+#include "Guild.h"
+#include "GuildMgr.h"
+#include "Chat.h"
 
 struct npc_gm_tailor : ScriptedAI
 {
@@ -10,7 +13,7 @@ public:
     {
         ClearGossipMenuFor(player);
 
-        AddGossipItemFor(player, GossipOptionIcon::None, "Give me a set of GM clothes.", 0, 1);
+        AddGossipItemFor(player, GossipOptionIcon::None, "I need a set of GM clothes.", 0, 1);
 
         SendGossipMenuFor(player, 802000, me);
 
@@ -27,7 +30,56 @@ public:
             player->AddItem(2586, 1);
             player->AddItem(11508, 1);
             player->AddItem(12064, 1);
+
+        ChatHandler(player).SendSysMessage("Do not give these robes to ANY non-GM players. This WILL result in a loss of GM privileges, and possibly a ban.");
         }
+
+        return true;
+    }
+};
+
+struct npc_gm_commander : ScriptedAI
+{
+public:
+    npc_gm_commander(Creature* creature) : ScriptedAI(creature) {}
+
+    bool OnGossipHello(Player* player) override
+    {
+        ClearGossipMenuFor(player);
+
+        AddGossipItemFor(player, GossipOptionIcon::None, "I want to join the GM Guild.", 0, 1, "Warning! This will remove you from any existing guilds!", 0, false);
+
+        SendGossipMenuFor(player, 802001, me);
+
+        return true;
+    }
+
+    bool OnGossipSelect(Player* player, uint32 menuid, uint32 gossipid) override
+    {
+        uint32 actionid = player->PlayerTalkClass->GetGossipOptionAction(gossipid);
+
+        CloseGossipMenuFor(player);
+
+        if (player->GetSession()->GetSecurity() < SEC_GAMEMASTER)
+        {
+            ChatHandler(player).SendSysMessage("You are not eligible to join this guild!");
+            return true;
+        }
+
+        if (Guild* guild = player->GetGuild())
+        {
+            if (guild->GetName() == "GM")
+            {
+                ChatHandler(player).SendSysMessage("You are already in the GM Guild!");
+                return true;
+            }
+
+            guild->HandleLeaveMember(player->GetSession());
+        }
+
+        Guild* gmGuild = sGuildMgr->GetGuildByName("GM");
+        if (gmGuild)
+            gmGuild->HandleAcceptMember(player->GetSession());
 
         return true;
     }
@@ -36,4 +88,6 @@ public:
 void AddSC_GMTavern()
 {
     RegisterCreatureAI(npc_gm_tailor);
+    RegisterCreatureAI(npc_gm_commander);
+
 }
