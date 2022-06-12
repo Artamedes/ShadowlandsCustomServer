8e27435ac99b4fbae24a1816abda4d2e9df66311
Artamedes <ownedned123@gmail.com>
2022-06-12 05:00:39 -0400

    f

--- src/server/scripts/Spells/Covenant/spell_nightfae.cpp
+++ src/server/scripts/Spells/Covenant/spell_nightfae.cpp
@@ -225,7 +225,7 @@ public:
                 case 259756:
                 case 259760:
                 case 259760:
-                    return false;
+                    return;
                 default:
                     break;
             }
