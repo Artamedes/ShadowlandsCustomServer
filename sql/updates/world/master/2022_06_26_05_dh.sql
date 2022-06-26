b632d76a211ad9ec6307c78db8ebe2bfbe321f29
Artamedes <ownedned123@gmail.com>
2022-06-26 04:51:01 -0400

    Scripts/Spells: Fix Deadly Dance (Havoc 4 set)

---
+++ sql/updates/world/master/2022_06_26_05_dh.sql
@@ -0,0 +1,4 @@
+-- DEADLY DANCE
+
+DELETE FROM spell_script_names WHERE spell_id = 363736;
+INSERT INTO spell_script_names (spell_id, scriptname) VALUE (363736, 'spell_deadly_dance');
--- src/server/scripts/Spells/Classes/DemonHunter/spell_dh_havoc.cpp
+++ src/server/scripts/Spells/Classes/DemonHunter/spell_dh_havoc.cpp
@@ -1,5 +1,10 @@
 #include "SpellIncludes.h"
 
+enum eHavoc
+{
+    Metamorphosis = 162264,
+};
+
 /// 21832
 struct at_glaive_tempest : public AreaTriggerAI
 {
@@ -24,7 +29,44 @@ public:
     }
 };
 
+/// ID: 363736 Deadly Dance
+class spell_deadly_dance : public AuraScript
+{
+    PrepareAuraScript(spell_deadly_dance);
+
+    bool CheckProc(ProcEventInfo& eventInfo)
+    {
+        return eventInfo.GetProcSpell() && eventInfo.GetProcSpell()->GetPowerCost(POWER_FURY);
+    }
+
+    void HandleProc(ProcEventInfo& eventInfo)
+    {
+        if (auto spell = eventInfo.GetProcSpell())
+        {
+            if (auto cost = eventInfo.GetProcSpell()->GetPowerCost(POWER_FURY))
+            {
+                usedFury += cost->Amount;
+                while (usedFury >= 60)
+                {
+                    usedFury -= 60;
+                    if (auto caster = GetCaster())
+                        caster->GetSpellHistory()->ModifyCooldown(Metamorphosis, -1000);
+                }
+            }
+        }
+    }
+
+    uint32 usedFury = 0;
+
+    void Register() override
+    {
+        DoCheckProc += AuraCheckProcFn(spell_deadly_dance::CheckProc);
+        OnProc += AuraProcFn(spell_deadly_dance::HandleProc);
+    }
+};
+
 void AddSC_spell_dh_havoc()
 {
     RegisterAreaTriggerAI(at_glaive_tempest);
+    RegisterSpellScript(spell_deadly_dance);
 }
