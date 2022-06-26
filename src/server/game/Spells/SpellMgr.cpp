36468c2c6ffc008c9ae3c0d10187b909e29da068
offl <11556157+offl@users.noreply.github.com>
2022-06-26 21:44:40 +0300

    Scripts/Spells: Correctly implement Murmur's Touch & Dire Brew (#28058)
    (cherry picked from commit f0d2418e5f2dabfae97a481fde52bc20ba93c72e)

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3764,7 +3764,9 @@ void SpellMgr::LoadSpellInfoCorrections()
         38762, // Force of Neltharaku
         51122, // Fierce Lightning Stike
         71848, // Toxic Wasteling Find Target
-        36146  // Chains of Naberius
+        36146, // Chains of Naberius
+        33711, // Murmur's Touch
+        38794  // Murmur's Touch
     }, [](SpellInfo* spellInfo)
     {
         spellInfo->MaxAffectedTargets = 1;
@@ -3837,16 +3839,6 @@ void SpellMgr::LoadSpellInfoCorrections()
     ApplySpellFix({ 50312 }, [](SpellInfo* spellInfo)
     {
         spellInfo->MaxAffectedTargets = 15;
-    });
-
-    // Murmur's Touch
-    ApplySpellFix({ 33711, 38794 }, [](SpellInfo* spellInfo)
-    {
-        spellInfo->MaxAffectedTargets = 1;
-        ApplySpellEffectFix(spellInfo, EFFECT_0, [](SpellEffectInfo* spellEffectInfo)
-        {
-            spellEffectInfo->TriggerSpell = 33760;
-        });
     });
 
     // Fingers of Frost
