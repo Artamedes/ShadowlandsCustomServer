3042ea6a3d4b12c9d0d59505ec58b09b5b8cb4eb
Artamedes <ownedned123@gmail.com>
2022-06-27 17:42:23 -0400

    Desmotareon spellclick

---
+++ sql/updates/world/master/2022_06_07_03.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES (707027, 268270, 1, 0);
+REPLACE INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES (707035, 18282, 0, 0);
+REPLACE INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES (707037, 18282, 0, 0);
