ad4a42efdb914846e9d46d26706d00366e247a67
Artamedes <ownedned123@gmail.com>
2022-06-29 09:53:47 -0400

    Remove disables when done

--- sql/updates/world/master/2022_06_29_01.sql
+++ sql/updates/world/master/2022_06_29_01.sql
@@ -4,3 +4,5 @@ INSERT INTO spell_script_names (spell_id, scriptname) VALUE (366792, 'spell_keg_
 
 DELETE FROM spell_script_names WHERE spell_id = 366779;
 INSERT INTO spell_script_names (spell_id, scriptname) VALUE (366779, 'spell_heal_the_soul');
+
+DELETE FROM disables WHERE sourcetype = 0 AND entry IN (207400, 207401, 366779);
