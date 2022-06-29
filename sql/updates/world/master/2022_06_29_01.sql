55ed4210006c8635c220f7fc9470835dedf06989
Artamedes <ownedned123@gmail.com>
2022-06-29 09:57:51 -0400

    a

--- sql/updates/world/master/2022_06_29_01.sql
+++ sql/updates/world/master/2022_06_29_01.sql
@@ -6,3 +6,6 @@ DELETE FROM spell_script_names WHERE spell_id = 366779;
 INSERT INTO spell_script_names (spell_id, scriptname) VALUE (366779, 'spell_heal_the_soul');
 
 DELETE FROM disables WHERE sourcetype = 0 AND entry IN (207400, 207401, 366779);
+
+DELETE FROM spell_script_names WHERE spell_id = 207401;
+INSERT INTO spell_script_names (spell_id, scriptname) VALUE (207401, 'spell_ancestral_vigor');
--- src/server/scripts/Spells/Classes/Shaman/spell_shaman_restoration.cpp
+++ src/server/scripts/Spells/Classes/Shaman/spell_shaman_restoration.cpp
@@ -2,7 +2,10 @@
 
 enum eRestoration
 {
-    ChainHeal = 1064,
+    HealingWave  = 77472,
+    HealingSurge = 8004,
+    Riptide      = 61295,
+    ChainHeal    = 1064,
 };
 
 /// ID: 366779 Heal the Soul
@@ -26,8 +29,36 @@ class spell_heal_the_soul : public AuraScript
         OnProc += AuraProcFn(spell_heal_the_soul::HandleProc);
     }
 };
+/// ID: 207401 Ancestral Vigor
+class spell_ancestral_vigor : public AuraScript
+{
+    PrepareAuraScript(spell_ancestral_vigor);
+
+    bool CheckProc(ProcEventInfo& eventInfo)
+    {
+        if (!eventInfo.GetSpellInfo())
+            return false;
+
+        switch (eventInfo.GetSpellInfo()->Id)
+        {
+            case HealingWave:
+            case HealingSurge:
+            case Riptide:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    void Register() override
+    {
+        DoCheckProc += AuraCheckProcFn(spell_ancestral_vigor::CheckProc);
+    }
+};
+
 
 void AddSC_spell_shaman_restoration()
 {
     RegisterSpellScript(spell_heal_the_soul);
+    RegisterSpellScript(spell_ancestral_vigor);
 }
