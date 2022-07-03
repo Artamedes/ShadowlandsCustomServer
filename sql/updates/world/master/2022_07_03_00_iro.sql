8c357914ae6611c8a752d7505522b15a31e356a0
Deveiro <deveirowow@gmail.com>
2022-07-03 00:08:59 -0400

    Scripts/Spells: (maybe) Fix Fueled by Violence Warrior Conduit

---
+++ sql/updates/world/master/2022_07_03_00_iro.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_script_names WHERE spell_id = 347213;
+INSERT INTO spell_script_names (spell_id, scriptname) VALUE (347213, 'spell_fueled_by_violence');
--- src/server/scripts/Spells/Classes/Warrior/spell_warrior_covenant.cpp
+++ src/server/scripts/Spells/Classes/Warrior/spell_warrior_covenant.cpp
@@ -2,11 +2,13 @@
 
 enum WarriorCovenant
 {
-    VeteransReputeAura = 339265,
-    VeteransReputeStr = 339267,
+    VeteransReputeAura  = 339265,
+    VeteransReputeStr   = 339267,
 
-    ExecuteDmg = 260798,
+    ExecuteDmg  = 260798,
     CondemmnDmg = 317483,
+
+    DeepWounds = 115767,
 };
 
 /// ID: 324143 Conqueror's Banner
@@ -146,6 +148,39 @@ class spell_hack_and_slash : public AuraScript
     }
 };
 
+/// ID: 347213 Fueled by Violence
+class spell_fueled_by_violence : public AuraScript
+{
+    PrepareAuraScript(spell_fueled_by_violence);
+
+    enum eFueledViolence
+    {
+        SpellProc = 347214,
+    };
+
+    bool CheckProc(ProcEventInfo& eventInfo)
+    {
+        if (!eventInfo.GetSpellInfo())
+            return false;
+
+        return eventInfo.GetSpellInfo()->Id == DeepWounds;
+    }
+
+    void HandleProc(ProcEventInfo& eventInfo)
+    {
+        if (auto caster = GetCaster())
+            if (auto effect = GetEffect(EFFECT_0))
+                if (effect->ConduitRankEntry)
+                    if (eventInfo.GetDamageInfo())
+                        caster->CastSpell(caster, SpellProc, CastSpellExtraArgs(true).AddSpellBP0(CalculatePct(eventInfo.GetDamageInfo()->GetDamage(), effect->ConduitRankEntry->AuraPointsOverride)));
+    }
+
+    void Register() override
+    {
+        DoCheckProc += AuraCheckProcFn(spell_fueled_by_violence::CheckProc);
+        OnProc += AuraProcFn(spell_fueled_by_violence::HandleProc);
+    }
+};
 
 void AddSC_spell_warrior_covenant()
 {
@@ -153,4 +188,5 @@ void AddSC_spell_warrior_covenant()
     RegisterSpellScript(spell_ashen_juggernaut);
     RegisterSpellScript(spell_indelible_victory);
     RegisterSpellScript(spell_hack_and_slash);
+    RegisterSpellScript(spell_fueled_by_violence);
 }
