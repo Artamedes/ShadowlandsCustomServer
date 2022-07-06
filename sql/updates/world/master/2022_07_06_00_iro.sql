91dff5a566857b627f8f4edaedbedcf799215861
Deveiro <deveirowow@gmail.com>
2022-07-06 19:21:56 -0400

    Scripts/Spells: Fix Resonant Words Priest Conduit

---
+++ sql/updates/world/master/2022_07_06_00_iro.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_script_names WHERE spell_id = 337947;
+INSERT INTO spell_script_names (spell_id, scriptname) VALUE (337947, 'spell_resonant_words');
--- src/server/scripts/Spells/Classes/Priest/spell_priest_covenant.cpp
+++ src/server/scripts/Spells/Classes/Priest/spell_priest_covenant.cpp
@@ -5,6 +5,14 @@ enum ePriest
     DesperatePrayer = 19236,
 };
 
+enum eHolyWords
+{
+    Salvation   = 265202,
+    Chastise    = 88625,
+    Sanctify    = 34861,
+    Serenity    = 2050
+};
+
 /// ID: 337748 Light's Inspiration
 class spell_lights_inspiration : public AuraScript
 {
@@ -38,7 +46,45 @@ class spell_lights_inspiration : public AuraScript
     }
 };
 
+/// ID: 337947 Resonant Words
+class spell_resonant_words : public AuraScript
+{
+    PrepareAuraScript(spell_resonant_words);
+
+    enum eResonantWords
+    {
+        ProcSpell = 337948,
+    };
+
+    bool CheckProc(ProcEventInfo& eventInfo)
+    {
+        if (!eventInfo.GetSpellInfo())
+            return false;
+
+        if (auto spell = eventInfo.GetSpellInfo()->Id)
+            if (spell == Salvation || spell == Chastise || spell == Sanctify || spell == Serenity)
+                return true;
+
+        return false;
+    }
+
+    void HandleProc(ProcEventInfo& eventInfo)
+    {
+        if (auto caster = GetCaster())
+            if (auto eff = GetEffect(EFFECT_0))
+                if (eff->ConduitRankEntry)
+                    caster->CastSpell(caster, ProcSpell, CastSpellExtraArgs(true).AddSpellBP0(eff->ConduitRankEntry->AuraPointsOverride));
+    }
+
+    void Register() override
+    {
+        DoCheckProc += AuraCheckProcFn(spell_resonant_words::CheckProc);
+        OnProc += AuraProcFn(spell_resonant_words::HandleProc);
+    }
+};
+
 void AddSC_spell_priest_covenant()
 {
     RegisterSpellScript(spell_lights_inspiration);
+    RegisterSpellScript(spell_resonant_words);
 }
