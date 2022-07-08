nbyaya/The-Cataclysm-Preservation-Project

Core/Movement: handle special follow movement case for companions

src/server/game/Movement/MotionMaster.cpp
@@ -255,7 +255,7 @@ void MotionMaster::MoveRandom(float spawndist)
     }
 }
 
-void MotionMaster::MoveFollow(Unit* target, float dist, float angle, bool joinFormation /*= false*/, bool catchUpToTarget /*= false*/, MovementSlot slot /*= MOTION_SLOT_IDLE*/)
+void MotionMaster::MoveFollow(Unit* target, float dist, float angle, bool joinFormation /*= false*/, bool catchUpToTarget /*= false*/, bool faceTarget /*= false*/, MovementSlot slot /*= MOTION_SLOT_IDLE*/)
 {
     // ignore movement request if target not exist
     if (!target || target == _owner)
@@ -263,7 +263,7 @@ void MotionMaster::MoveFollow(Unit* target, float dist, float angle, bool joinFo
 
     TC_LOG_DEBUG("movement.motionmaster", "MotionMaster::MoveFollow: '%s', starts following '%s'", _owner->GetGUID().ToString().c_str(), target->GetGUID().ToString().c_str());
 
-    Mutate(new FollowMovementGenerator(target, dist, angle, joinFormation, catchUpToTarget), slot);
+    Mutate(new FollowMovementGenerator(target, dist, angle, joinFormation, catchUpToTarget, faceTarget), slot);
 }
 
 void MotionMaster::MoveChase(Unit* target, float dist, Optional<ChaseAngle> angle)

src/server/game/Movement/MotionMaster.h
@@ -147,7 +147,7 @@ class TC_GAME_API MotionMaster
         void MoveIdle();
         void MoveTargetedHome();
         void MoveRandom(float spawndist = 0.0f);
-        void MoveFollow(Unit* target, float dist, float angle, bool joinFormation = false, bool catchUpToTarget = false, MovementSlot slot = MOTION_SLOT_IDLE);
+        void MoveFollow(Unit* target, float dist, float angle, bool joinFormation = false, bool catchUpToTarget = false, bool faceTarget = false, MovementSlot slot = MOTION_SLOT_IDLE);
 
         void MoveChase(Unit* target, float dist = 0.f, Optional<ChaseAngle> angle = {});
         void MoveChase(Unit* target, float dist, float angle) { MoveChase(target, dist, ChaseAngle(angle)); }

Artamedes/ShadowlandsCustomServer

src/server/game/Movement/MotionMaster.cpp
@@ -622,14 +622,14 @@ void MotionMaster::MoveRandom(float wanderDistance)
     }
 }
 
-void MotionMaster::MoveFollow(Unit* target, float dist, float angle, bool joinFormation /*= false*/, bool catchUpToTarget /*= false*/, MovementSlot slot /*= MOTION_SLOT_IDLE*/)
+void MotionMaster::MoveFollow(Unit* target, float dist, float angle, bool joinFormation /*= false*/, bool catchUpToTarget /*= false*/, bool faceTarget /*= false*/, MovementSlot slot /*= MOTION_SLOT_IDLE*/)
 {
     // Ignore movement request if target not exist
     if (!target || target == _owner)
         return;
 
     TC_LOG_DEBUG("movement.motionmaster", "MotionMaster::MoveFollow: '%s', starts following '%s'", _owner->GetGUID().ToString().c_str(), target->GetGUID().ToString().c_str());
-    Add(new FollowMovementGenerator(target, dist, angle, joinFormation, catchUpToTarget), slot);
+    Add(new FollowMovementGenerator(target, dist, angle, joinFormation, catchUpToTarget, faceTarget), slot);
 }
 
 void MotionMaster::MoveChase(Unit* target, Optional<ChaseRange> dist, Optional<ChaseAngle> angle)

src/server/game/Movement/MotionMaster.h
@@ -185,7 +185,7 @@ class TC_GAME_API MotionMaster
         void MoveTargetedHome();
         MovementGenerator* MoveTargetedHomeForce();
         void MoveRandom(float wanderDistance = 0.0f);
-        void MoveFollow(Unit* target, float dist, float angle, bool joinFormation = false, bool catchUpToTarget = false, MovementSlot slot = MOTION_SLOT_DEFAULT);
+        void MoveFollow(Unit* target, float dist, float angle, bool joinFormation = false, bool catchUpToTarget = false, bool faceTarget = false, MovementSlot slot = MOTION_SLOT_DEFAULT);
         void MoveChase(Unit* target, Optional<ChaseRange> dist = {}, Optional<ChaseAngle> angle = {});
         void MoveChase(Unit* target, float dist, float angle) { MoveChase(target, ChaseRange(dist), ChaseAngle(angle)); }
         void MoveChase(Unit* target, float dist) { MoveChase(target, ChaseRange(dist)); }