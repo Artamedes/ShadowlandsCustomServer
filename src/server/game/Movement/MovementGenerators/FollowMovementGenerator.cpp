nbyaya/The-Cataclysm-Preservation-Project

Core/Units: changed _formationFollowers from unit references to object guids to have a safer access to its stored units

src/server/game/Movement/MovementGenerators/FollowMovementGenerator.cpp
@@ -274,19 +274,22 @@ bool FollowMovementGenerator::Update(Unit* owner, uint32 diff)
 void FollowMovementGenerator::UpdateFollowFormation()
 {
     uint8 followSlot = 0;
-    for (Unit* follower : _target->GetFormationFollowers())
+    for (ObjectGuid guid : _target->GetFormationFollowers())
     {
-        for (uint8 slot = MOTION_SLOT_IDLE; slot < MAX_MOTION_SLOT; ++slot)
+        if (Unit* follower = ObjectAccessor::GetUnit(*_target, guid))
         {
-            MovementGenerator* moveGen = follower->GetMotionMaster()->GetMotionSlot(MovementSlot(slot));
-            if (!moveGen || moveGen->GetMovementGeneratorType() != FOLLOW_MOTION_TYPE)
-                continue;
+            for (uint8 slot = MOTION_SLOT_IDLE; slot < MAX_MOTION_SLOT; ++slot)
+            {
+                MovementGenerator* moveGen = follower->GetMotionMaster()->GetMotionSlot(MovementSlot(slot));
+                if (!moveGen || moveGen->GetMovementGeneratorType() != FOLLOW_MOTION_TYPE)
+                    continue;
 
-            if (FollowMovementGenerator* followMoveGen = static_cast<FollowMovementGenerator*>(moveGen))
-                followMoveGen->UpdateFormationFollowOffsets(followSlot);
+                if (FollowMovementGenerator* followMoveGen = static_cast<FollowMovementGenerator*>(moveGen))
+                    followMoveGen->UpdateFormationFollowOffsets(followSlot);
+            }
+
+            ++followSlot;
         }
-
-        ++followSlot;
     }
 }
 

Artamedes/ShadowlandsCustomServer

src/server/game/Movement/MovementGenerators/FollowMovementGenerator.cpp
@@ -290,19 +290,21 @@ bool FollowMovementGenerator::Update(Unit* owner, uint32 diff)
 void FollowMovementGenerator::UpdateFollowFormation()
 {
     uint8 followSlot = 0;
-    for (Unit* follower : _target->GetFormationFollowers())
+    for (ObjectGuid guid : _target->GetFormationFollowers())
     {
-        for (uint8 slot = MOTION_SLOT_DEFAULT; slot < MAX_MOTION_SLOT; ++slot)
+        if (Unit* follower = ObjectAccessor::GetUnit(*_target, guid))
         {
-            MovementGenerator* moveGen = follower->GetMotionMaster()->GetCurrentMovementGenerator(MovementSlot(slot));
-            if (!moveGen || moveGen->GetMovementGeneratorType() != FOLLOW_MOTION_TYPE)
-                continue;
+            for (uint8 slot = MOTION_SLOT_DEFAULT; slot < MAX_MOTION_SLOT; ++slot)
+            {
+                MovementGenerator* moveGen = follower->GetMotionMaster()->GetCurrentMovementGenerator(MovementSlot(slot));
+                if (!moveGen || moveGen->GetMovementGeneratorType() != FOLLOW_MOTION_TYPE)
+                    continue;
 
-            if (FollowMovementGenerator* followMoveGen = static_cast<FollowMovementGenerator*>(moveGen))
-                followMoveGen->UpdateFormationFollowOffsets(followSlot);
+                if (FollowMovementGenerator* followMoveGen = static_cast<FollowMovementGenerator*>(moveGen))
+                    followMoveGen->UpdateFormationFollowOffsets(followSlot);
+            }
+            ++followSlot;
         }
-
-        ++followSlot;
     }
 }
 