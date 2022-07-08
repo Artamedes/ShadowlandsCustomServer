nbyaya/The-Cataclysm-Preservation-Project

Core/Movement: reimplement follow movement (#124)

src/server/game/Movement/MotionMaster.h
@@ -147,7 +147,7 @@ class TC_GAME_API MotionMaster
         void MoveIdle();
         void MoveTargetedHome();
         void MoveRandom(float spawndist = 0.0f);
-        void MoveFollow(Unit* target, float dist, ChaseAngle angle, bool allignToTargetSpeed = false, MovementSlot slot = MOTION_SLOT_ACTIVE);
+        void MoveFollow(Unit* target, float dist, float angle, bool joinFormation = false, bool catchUpToTarget = false, MovementSlot slot = MOTION_SLOT_IDLE);
 
         void MoveChase(Unit* target, float dist = 0.f, Optional<ChaseAngle> angle = {});
         void MoveChase(Unit* target, float dist, float angle) { MoveChase(target, dist, ChaseAngle(angle)); }

Artamedes/ShadowlandsCustomServer

src/server/game/Movement/MotionMaster.h
@@ -185,7 +185,7 @@ class TC_GAME_API MotionMaster
         void MoveTargetedHome();
         MovementGenerator* MoveTargetedHomeForce();
         void MoveRandom(float wanderDistance = 0.0f);
-        void MoveFollow(Unit* target, float dist, ChaseAngle angle, MovementSlot slot = MOTION_SLOT_ACTIVE);
+        void MoveFollow(Unit* target, float dist, float angle, bool joinFormation = false, bool catchUpToTarget = false, MovementSlot slot = MOTION_SLOT_DEFAULT);
         void MoveChase(Unit* target, Optional<ChaseRange> dist = {}, Optional<ChaseAngle> angle = {});
         void MoveChase(Unit* target, float dist, float angle) { MoveChase(target, ChaseRange(dist), ChaseAngle(angle)); }
         void MoveChase(Unit* target, float dist) { MoveChase(target, ChaseRange(dist)); }