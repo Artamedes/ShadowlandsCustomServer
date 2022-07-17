051a0050bdec25bf8f1c5982ca8f4631071a25ed
Artamedes <ownedned123@gmail.com>
2022-07-17 16:28:55 -0400

    Add proc flags for last commit

--- sql/updates/world/master/2022_07_17_01_paladin.sql
+++ sql/updates/world/master/2022_07_17_01_paladin.sql
@@ -35,3 +35,6 @@ INSERT INTO spell_script_names (spell_id, scriptname) VALUE (337600, 'spell_uthe
 
 DELETE FROM spell_script_names WHERE spell_id = 317866;
 INSERT INTO spell_script_names (spell_id, scriptname) VALUE (317866, 'spell_sanctified_wrath');
+DELETE FROM `spell_proc` WHERE `SpellId` IN (317866);
+INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
+(317866,0x00,10,0x00000000,0x00000000,0x00000000,0x00000000,0x0,0x4,0x0,0x2,0x0,0x0,0x0,0,0,0,0); -- Sanctified Wrath
