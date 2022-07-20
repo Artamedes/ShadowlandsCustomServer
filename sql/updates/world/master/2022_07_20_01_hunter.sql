6ac7187bebcc7eb05eeafcfe902b4e14babc8bc0
Artamedes <ownedned123@gmail.com>
2022-07-20 19:11:14 -0400

    Scripts/Spells: Fix MM Dead Eye

---
+++ sql/updates/world/master/2022_07_20_01_hunter.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_script_names WHERE spell_id = 321460;
+INSERT INTO spell_script_names (spell_id, scriptname) VALUE (321460, 'spell_dead_eye');
--- src/server/scripts/Spells/Classes/Hunter/spell_hunter_marksmanship.cpp
+++ src/server/scripts/Spells/Classes/Hunter/spell_hunter_marksmanship.cpp
@@ -1,4 +1,6 @@
-#include "SpellIncludes.h"
+#include "spell_hunter.h"
+
+using namespace Hunter;
 
 /// ID: 260309 Master Marksman
 class spell_master_marksman : public AuraScript
@@ -35,8 +37,24 @@ class spell_master_marksman : public AuraScript
         OnProc += AuraProcFn(spell_master_marksman::HandleProc);
     }
 };
+/// ID: 321460 Dead Eye
+class spell_dead_eye : public AuraScript
+{
+    PrepareAuraScript(spell_dead_eye);
+
+    bool CheckProc(ProcEventInfo& eventInfo)
+    {
+        return eventInfo.GetSpellInfo() && eventInfo.GetSpellInfo()->Id == KillShot;
+    }
+
+    void Register() override
+    {
+        DoCheckProc += AuraCheckProcFn(spell_dead_eye::CheckProc);
+    }
+};
 
 void AddSC_spell_hunter_marksman()
 {
     RegisterSpellScript(spell_master_marksman);
+    RegisterSpellScript(spell_dead_eye);
 }
