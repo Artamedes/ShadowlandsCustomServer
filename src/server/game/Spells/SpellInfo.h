nbyaya/The-Cataclysm-Preservation-Project

Core/Spells: define and implement SPELL_ATTR8_PERIODIC_CAN_CRIT
* SPELL_ATTR8_DONT_RESET_PERIODIC_TIMER has been moved to a custom attribute (SPELL_ATTR0_CU_DONT_RESET_PERIODIC)

src/server/game/Spells/SpellInfo.h
@@ -200,6 +200,7 @@ enum SpellCustomAttributes
     SPELL_ATTR0_CU_BINARY_SPELL                  = 0x00100000,
     SPELL_ATTR0_CU_SCHOOLMASK_NORMAL_WITH_MAGIC  = 0x00200000,
     SPELL_ATTR0_CU_LIQUID_AURA                   = 0x00400000,
+    SPELL_ATTR0_CU_DONT_RESET_PERIODIC           = 0x00800000,
 
     SPELL_ATTR0_CU_NEGATIVE                      = SPELL_ATTR0_CU_NEGATIVE_EFF0 | SPELL_ATTR0_CU_NEGATIVE_EFF1 | SPELL_ATTR0_CU_NEGATIVE_EFF2
 };

Artamedes/ShadowlandsCustomServer

src/server/game/Spells/SpellInfo.h
@@ -172,6 +172,7 @@ enum SpellCustomAttributes
     SPELL_ATTR0_CU_DEPRECATED_LIQUID_AURA        = 0x00400000, // DO NOT REUSE
     SPELL_ATTR0_CU_IS_TALENT                     = 0x00800000,
     SPELL_ATTR0_CU_AURA_CANNOT_BE_SAVED          = 0x01000000,
+    SPELL_ATTR0_CU_DONT_RESET_PERIODIC           = 0x02000000,
 };
 
 uint32 GetTargetFlagMask(SpellTargetObjectTypes objType);