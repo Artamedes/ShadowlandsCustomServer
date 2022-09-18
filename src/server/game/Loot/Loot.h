3e37f022be686993c1922348210587956c53c44c
Shauren <shauren.trinity@gmail.com>
2022-09-18 18:24:57 +0200

    Core/Loot: Refactored Player::SendLoot to accept Loot object directly

--- src/server/game/Loot/Loot.h
+++ src/server/game/Loot/Loot.h
@@ -192,6 +192,12 @@ struct TC_GAME_API LootItem
                  freeforall(false), is_underthreshold(false), is_counted(false), needs_quest(false), follow_loot_rules(false), personal(false),
                  currentLoot(nullptr) { };
 
+    LootItem(LootItem const&);
+    LootItem(LootItem&&) noexcept;
+    LootItem& operator=(LootItem const&);
+    LootItem& operator=(LootItem&&) noexcept;
+    ~LootItem();
+
     // Basic checks for player/item compatibility - if false no chance to see the item in the loot
     bool AllowedForPlayer(Player const* player, bool isGivenByMasterLooter = false) const;
     void AddAllowedLooter(Player const* player);
