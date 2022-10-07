allankrdec/TrinityCore-10-2024

Core/Gossip: Drop OptionType for gossip_menu_option and rename OptionIcon to OptionNpc as it also determines functionality on client side (#28220)
Co-authored-by: Shauren <shauren.trinity@gmail.com>

src/server/game/Server/Packets/NPCPackets.h
@@ -24,7 +24,7 @@
 #include "Position.h"
 #include <array>
 
-enum class GossipOptionIcon : uint8;
+enum class GossipOptionNpc : uint8;
 enum class GossipOptionStatus : uint8;
 enum class GossipOptionRewardType : uint8;
 
@@ -64,7 +64,7 @@ namespace WorldPackets
         struct ClientGossipOptions
         {
             int32 ClientOption  = 0;
-            GossipOptionIcon OptionNPC = GossipOptionIcon(0);
+            GossipOptionNpc OptionNPC = GossipOptionNpc(0);
             uint8 OptionFlags   = 0;
             int32 OptionCost    = 0;
             uint32 OptionLanguage = 0;

Artamedes/ShadowlandsCustomServer

src/server/game/Server/Packets/NPCPackets.h
@@ -24,7 +24,7 @@
 #include "Position.h"
 #include <array>
 
-enum class GossipOptionIcon : int8;
+enum class GossipOptionNpc : int8;
 enum class GossipOptionStatus : uint8;
 enum class GossipOptionRewardType : uint8;
 enum class PlayerInteractionType : uint32;
@@ -65,7 +65,7 @@ namespace WorldPackets
         struct ClientGossipOptions
         {
             int32 ClientOption  = 0;
-            GossipOptionIcon OptionNPC = GossipOptionIcon(0);
+            GossipOptionNpc OptionNPC = GossipOptionNpc(0);
             uint8 OptionFlags   = 0;
             int32 OptionCost    = 0;
             uint32 OptionLanguage = 0;