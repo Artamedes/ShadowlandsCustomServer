30eda88c5797120994b5061f728203c2ca825304
Artamedes <ownedned123@gmail.com>
2022-11-29 18:55:18 -0500

    This neeeds to copy it seems

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11096,8 +11096,10 @@ void Unit::GetProcAurasTriggeredOnEvent(AuraApplicationProcContainer& aurasTrigg
     // or generate one on our own
     else
     {
-        for (AuraApplicationMap::iterator itr = GetAppliedAuras().begin(); itr != GetAppliedAuras().end(); ++itr)
-            processAuraApplication(itr->second);
+        AuraApplicationMap appliedAuras = m_appliedAuras;
+        for (AuraApplicationMap::iterator itr = appliedAuras.begin(); itr != appliedAuras.end(); ++itr)
+            if (itr->second)
+                processAuraApplication(itr->second);
     }
 }
 
