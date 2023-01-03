11f4bbbaa036bba2f1a20ed374f0b800464c1c49
Aqua Deus <95978183+aquadeus@users.noreply.github.com>
2023-01-03 18:30:51 +0100

    Scripts/RLP: Added initial spawns and some cosmetic spells (#28629)

--- src/server/scripts/DragonIsles/dragon_isles_script_loader.cpp
+++ src/server/scripts/DragonIsles/dragon_isles_script_loader.cpp
@@ -1,5 +1,11 @@
 
+// Ruby Life Pools
+void AddSC_instance_ruby_life_pools();
+void AddSC_ruby_life_pools();
+
 void AddDragonIslesScripts()
 {
-
+    // Ruby Life Pools
+    AddSC_instance_ruby_life_pools();
+    AddSC_ruby_life_pools();
 }
