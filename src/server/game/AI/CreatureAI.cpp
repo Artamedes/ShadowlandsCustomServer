28f9921f69bc552ac05ef5a103719ba7d73c7890
Shauren <shauren.trinity@gmail.com>
2023-01-06 16:44:11 +0100

    Core/Misc: Add missing Containers.h include where neccessary

--- src/server/game/AI/CreatureAI.cpp
+++ src/server/game/AI/CreatureAI.cpp
@@ -34,6 +34,7 @@
 #include "SpellHistory.h"
 #include "TemporarySummon.h"
 #include "Vehicle.h"
+#include <queue>
 
 std::unordered_map<std::pair<uint32, Difficulty>, AISpellInfoType> UnitAI::AISpellInfo;
 AISpellInfoType* GetAISpellInfo(uint32 spellId, Difficulty difficulty)
