allankrdec/TrinityCore-10-2024

Core/GameObjects: Use serverOnly property of GameObjectTemplate for all types that have it, not just spell focus

src/server/game/Entities/GameObject/GameObjectData.h
@@ -617,6 +617,18 @@ struct GameObjectTemplate
         }
     }
 
+    uint32 GetServerOnly() const
+    {
+        switch (type)
+        {
+            case GAMEOBJECT_TYPE_GENERIC: return _generic.serverOnly;
+            case GAMEOBJECT_TYPE_TRAP: return trap.serverOnly;
+            case GAMEOBJECT_TYPE_SPELL_FOCUS: return spellFocus.serverOnly;
+            case GAMEOBJECT_TYPE_AURA_GENERATOR: return auraGenerator.serverOnly;
+            default: return 0;
+        }
+    }
+
     void InitializeQueryData();
     WorldPacket BuildQueryData(LocaleConstant loc) const;
 };

Artamedes/ShadowlandsCustomServer

(cherry picked from commit 4ee64a232ad830255bc6779e595d412c934b6b74)

src/server/game/Entities/GameObject/GameObjectData.h
@@ -1247,6 +1247,18 @@ struct GameObjectTemplate
         }
     }
 
+    uint32 GetServerOnly() const
+    {
+        switch (type)
+        {
+            case GAMEOBJECT_TYPE_GENERIC: return generic.serverOnly;
+            case GAMEOBJECT_TYPE_TRAP: return trap.serverOnly;
+            case GAMEOBJECT_TYPE_SPELL_FOCUS: return spellFocus.serverOnly;
+            case GAMEOBJECT_TYPE_AURA_GENERATOR: return auraGenerator.serverOnly;
+            default: return 0;
+        }
+    }
+
     uint32 GetSpellFocusType() const
     {
         switch (type)