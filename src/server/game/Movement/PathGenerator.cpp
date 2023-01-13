allankrdec/TrinityCore-10-2024

Core/Maps: Fixed retrieving terrain info when on child maps

src/server/game/Movement/PathGenerator.cpp
@@ -38,7 +38,7 @@ PathGenerator::PathGenerator(WorldObject const* owner) :
 
     TC_LOG_DEBUG("maps.mmaps", "++ PathGenerator::PathGenerator for %s", _source->GetGUID().ToString().c_str());
 
-    uint32 mapId = PhasingHandler::GetTerrainMapId(_source->GetPhaseShift(), _source->GetMap()->GetTerrain(), _source->GetPositionX(), _source->GetPositionY());
+    uint32 mapId = PhasingHandler::GetTerrainMapId(_source->GetPhaseShift(), _source->GetMapId(), _source->GetMap()->GetTerrain(), _source->GetPositionX(), _source->GetPositionY());
     if (DisableMgr::IsPathfindingEnabled(_source->GetMapId()))
     {
         MMAP::MMapManager* mmap = MMAP::MMapFactory::createOrGetMMapManager();

Artamedes/ShadowlandsCustomServer

src/server/game/Movement/PathGenerator.cpp
@@ -55,7 +55,7 @@ PathGenerator::PathGenerator(const WorldObject* owner, bool transformTransportPa
     }
     else
     {
-        uint32 mapId = PhasingHandler::GetTerrainMapId(_sourceObj->GetPhaseShift(), _sourceObj->GetMap()->GetTerrain(), _sourceObj->GetPositionX(), _sourceObj->GetPositionY());
+        uint32 mapId = PhasingHandler::GetTerrainMapId(_sourceObj->GetPhaseShift(), _sourceObj->GetMapId(), _sourceObj->GetMap()->GetTerrain(), _sourceObj->GetPositionX(), _sourceObj->GetPositionY());
         if (DisableMgr::IsPathfindingEnabled(mapId))
         {
             MMAP::MMapManager* mmap = MMAP::MMapFactory::createOrGetMMapManager();