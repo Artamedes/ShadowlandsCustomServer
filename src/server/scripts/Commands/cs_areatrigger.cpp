7feaba6108230c370df2d0a624e4760d32afdc50
Artamedes <ownedned123@gmail.com>
2023-03-07 04:58:25 -0500

    Scripts/Commands: Working on areatrigger commands

---
+++ src/server/scripts/Commands/cs_areatrigger.cpp
@@ -0,0 +1,35 @@
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "Chat.h"
+#include "ChatCommand.h"
+#include "Player.h"
+#include "ScriptMgr.h"
+
+using namespace Trinity::ChatCommands;
+
+
+//class misc_commandscript : public CommandScript
+//{
+//public:
+//    misc_commandscript() : CommandScript("misc_commandscript") { }
+//
+//    ChatCommandTable GetCommands() const override
+//    {
+//        static ChatCommandTable commandTable =
+//        {
+//            { "at",    HandleAreaTriggerMenuCommand, rbac::RBAC_PERM_COMMAND_DEV,            Console::No },
+//        };
+//
+//        return commandTable;
+//    }
+//
+//    bool HandleAreaTriggerMenuCommand(ChatHandler* handler)
+//    {
+//        return true;
+//    }
+//};
+
+void AddSC_cs_areatrigger()
+{
+
+}
