64a9223595bad2ec565ee01c403e8bfd973bde9c
Shauren <shauren.trinity@gmail.com>
2023-03-26 12:22:41 +0200

    Core/Movement: Allow passing tier transition id (db2 id) to land/takeoff movement

--- src/server/game/Movement/MotionMaster.h
+++ src/server/game/Movement/MotionMaster.h
@@ -203,7 +203,10 @@ class TC_GAME_API MotionMaster
          */
         void MoveCloserAndStop(uint32 id, Unit* target, float distance);
         // These two movement types should only be used with creatures having landing/takeoff animations
-        GenericMovementGenerator* MoveLand(uint32 id, Position const& pos, Optional<float> velocity = {});
+        void MoveLand(uint32 id, Position const& pos, Optional<int32> tierTransitionId = {}, Optional<float> velocity = {},
+            MovementWalkRunSpeedSelectionMode speedSelectionMode = MovementWalkRunSpeedSelectionMode::Default);
+        void MoveTakeoff(uint32 id, Position const& pos, Optional<int32> tierTransitionId = {}, Optional<float> velocity = {},
+            MovementWalkRunSpeedSelectionMode speedSelectionMode = MovementWalkRunSpeedSelectionMode::Default);
         void MoveTakeoff(uint32 id, Position const& pos, Optional<float> velocity = {});
         void MoveCharge(float x, float y, float z, float speed = SPEED_CHARGE, uint32 id = EVENT_CHARGE, bool generatePath = false, Unit const* target = nullptr, Movement::SpellEffectExtraData const* spellEffectExtraData = nullptr);
         void MoveCharge(PathGenerator const& path, float speed = SPEED_CHARGE, Unit const* target = nullptr, Movement::SpellEffectExtraData const* spellEffectExtraData = nullptr);
